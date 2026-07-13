fe34e7fbba45eaecedfe8db814f1f43a662bc5b6
Abdo Elhlwy <abdoelhlwy@gmail.com>
2026-07-12 20:38:45 -0700

    fix config

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,21 +1,6 @@
-import type { NextConfig } from "next";
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference path="./.next/types/routes.d.ts" />
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**" }
-    ]
-  },
-  experimental: {
-    serverActions: { bodySizeLimit: "10mb" }
-  }
-};
-
-export default nextConfig;
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
